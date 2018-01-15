cc4346dd7b59bf1f1cfa09306c10f385f1286958
Stefan Christiansson <stefan.christiansso@learnet.se>
2018-01-15 13:08:46 +0100

    Created UDP Listener method

--- StefanChat/StefanChat/Program.cs
+++ StefanChat/StefanChat/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,36 @@ namespace StefanChat
 
     class Program
     {
+        private const int ListenPort = 11000;
+
         static void Main(string[] args)
         {
+
+        }
+
+        static void Listener()
+        {
+            var listener = new UdpClient(ListenPort);
+
+            try
+            {
+                while (true)
+                {
+                    var groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                    var bytes = listener.Receive(ref groupEP);
+                    Console.WriteLine("Revieved broadcast from {0} : {1}\n", groupEP.ToString(),
+                        Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+            finally
+            {
+                listener.Close();
+            }
         }
     }
 }
