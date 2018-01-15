b699ff23ec458327272144eb6ab40bc823415508
Stefan Christiansson <stefan.christiansso@learnet.se>
2018-01-15 12:17:13 +0100

    Added made by comment

---
+++ StefanChat/StefanChat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StefanChat
+{
+    //By Stefan Christiansson 2018-01-15
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
