11a8424ea6cc2e379b257dcaff0d04d67af522b7
Stefan Christiansson <stefan.christiansso@learnet.se>
2018-01-16 13:42:51 +0100

    Can read but typing not working

--- StefanChat/StefanChat/Program.cs
+++ StefanChat/StefanChat/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StefanChat
@@ -16,7 +17,25 @@ namespace StefanChat
 
         static void Main(string[] args)
         {
+            //Skapa och starta en tråd som körs samtidigt med resten av progammet
+            var listenThread = new Thread(Listener);
+            listenThread.Start();
 
+            //Skapa en anslutning för att kunna skicka meddelanden
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.EnableBroadcast = true;
+            var ep = new IPEndPoint(IPAddress.Broadcast, ListenPort);
+
+            while (true)
+            {
+                Console.Write(">");
+                var msg = Console.ReadLine();
+
+                var sendbuf = Encoding.UTF8.GetBytes(msg);
+                socket.SendTo(sendbuf, ep);
+
+                Thread.Sleep(200);
+            }
         }
 
         static void Listener()
